c5b8dab07c0303211be76c141d2061564a252a31
ssk-575 <86309194+ssk-575@users.noreply.github.com>
2021-10-16 12:27:53 +0530

    Add files via upload

---
+++ book my show clone/planning.js
@@ -0,0 +1,21 @@
+// book my show planning
+
+//-> we need a reusable carousel
+//->HERO carousel
+//->common carousel
+//->cast carousel (small)
+
+//pages
+
+//->home page
+//->movie page
+//->categories page
+//->auth page
+//->razorpay payment page
+
+// plan how we are building it ?
+
+//layouts
+//Routes
+//pages
+//components
